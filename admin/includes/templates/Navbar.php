e40d3c4a6e71fdefd359d9398928db31165e05a6
MahmodYousry <yousry.mohmmed@rocketmail.com>
2022-11-05 21:30:59 +0200

    categories into brand in admin page

--- admin/includes/templates/Navbar.php
+++ admin/includes/templates/Navbar.php
@@ -12,7 +12,7 @@
 
     <div class="collapse navbar-collapse" id="app-nav">
       <ul class="nav navbar-nav">
-        <li><a href="categories.php"><?php echo lang('BRAND'); ?></a></li>
+        <li><a href="brand.php"><?php echo lang('BRAND'); ?></a></li>
         <li><a href="type.php"><?php echo lang('TYPE'); ?></a></li>
         <li><a href="items.php"><?php echo lang('ITEMS'); ?></a></li>
         <li><a href="members.php"><?php echo lang('MEMBERS'); ?></a></li>
