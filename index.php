5f717aaa3463557e265c233f6b74d222c7620676
MahmodYousry <yousry.mohmmed@rocketmail.com>
2022-11-05 10:04:54 +0200

    fix front products

--- index.php
+++ index.php
@@ -11,25 +11,41 @@
 	<div class="row">
 		<?php 
 			// get item info with no image
-			$stmt = $con->prepare("SELECT * FROM items WHERE Approve = 1 ORDER BY Item_ID DESC");
+			$stmt = $con->prepare("SELECT * FROM items 
+					JOIN brand ON brand.id = items.brand_id
+					JOIN type ON type.type_id = items.type_id
+					ORDER BY Item_ID DESC");
 			$stmt->execute();
 			$allItems = $stmt->fetchAll();
 
 			foreach ($allItems as $item) {
 
+				echo $item[1];
+
 				// get item images by item id
-				$stmtimg = $con->prepare("SELECT * FROM item_imgs WHERE item_ID  = ?");
+				$stmtimg = $con->prepare("SELECT * FROM item_imgs WHERE item_ID = ?");
 				$stmtimg->execute([$item['Item_ID']]);
 				$itemImgs = $stmtimg->fetch();
+
+				if (isset($itemImgs[1])) {
+					$firstImg = $itemImgs[1];
+				} else {
+					$firstImg = 'def.webp';
+				}
 
 				echo '<div class="col-sm-6 col-md-3">';
 					echo '<div class="thumbnail item-box">';
 						echo '<span class="price-tag">جنيه ' . $item['price'] . '</span>';
-						echo '<a href="items.php?itemid=' . $item['Item_ID'] . '"><img class="img-responsive product-img" src="products/' . $itemImgs[1] . '" alt="" /></a>';
+						echo '<a href="items.php?itemid=' . $item['Item_ID'] . '">';
+							echo '<img class="img-responsive product-img" src="products/' . $firstImg . '" alt="" />';
+						echo '</a>';
 						echo '<div class="caption">';
-							echo '<h3 class="text-center"><a href="items.php?itemid=' . $item['Item_ID'] . '">' . $item['item_name'] . '</a></h3>';
-							echo '<p class="text-center">' . $item['number'] . '</p>';
-							echo '<p class="text-center">' . $item['Member_ID '] . '</p>';
+							echo '<h3 class="text-center">';
+								echo '<a href="items.php?itemid=' . $item['Item_ID'] . '">' . $item['item_name'] . '</a>';
+							echo '</h3>';
+							echo '<p class="text-center">We Have ' . $item['number'] . ' Items</p>';
+							echo '<p class="text-center">' . $item['name'] . '</p>';
+							echo '<p class="text-center">' . $item['type_name'] . '</p>';
 							echo '<div class="date">' . $item['Last_price_date'] . '</div>';
 						echo '</div>';
 					echo '</div>';
